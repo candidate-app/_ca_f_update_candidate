d3683fa9ef9b6c561c6a768a89c53ab1562c8bd4
Jishnu <jishnu.nb@neoito.com>
2022-05-03 15:12:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const f_update_candidate = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello f_update_candidate`}))
+  res.end()
+  
+}
+
+export default f_update_candidate
